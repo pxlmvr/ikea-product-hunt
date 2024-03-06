aa5e89a3a698cd70d4eefa060c4993413fc859d1
pxlmvr <matteo.losi@casavo.com>
2024-03-06 15:26:30 +0100

    refactor: remove handmade pwa configs

--- public/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', () => {
-  //console.log('Service Worker installing.')
-})
-
-self.addEventListener('fetch', (event) => {
-  //console.log('Fetching:', event.request.url)
-})
